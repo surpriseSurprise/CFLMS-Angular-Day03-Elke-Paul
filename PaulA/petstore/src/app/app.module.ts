bb7459b91824657d6d5a88577611a8fecd5bbf34
Paulalbo <65129276+Paulalbo@users.noreply.github.com>
2020-06-24 15:06:21 +0200

    added some fancy styling stuff

--- PaulA/petstore/src/app/app.module.ts
+++ PaulA/petstore/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     ProductListComponent,
     NavbarComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
